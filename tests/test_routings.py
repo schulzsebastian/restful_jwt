2eaf235293fc4d7de141f21b2d16d1ad7b194bd3
Sebastian Schulz <schulz.siwy@gmail.com>
2017-04-13 00:13:55 +0200

    flask-testing native json

--- tests/test_routings.py
+++ tests/test_routings.py
@@ -6,13 +6,12 @@ import sys, os.path
 sys.path.append(os.path.abspath('../'))
 
 import unittest
-import json
 from tests import BaseTest
 
 class TestRoutings(BaseTest):
 
     def test_status(self):
         response = self.client.get('/status')
-        data = json.loads(response.data.decode('utf-8'))
+        data = response.json
         self.assertIn('status', data)
         self.assertEqual(data['status'], 'ok')
